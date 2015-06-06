37a51ec64c9441b3c2ef402083a4282c1c84ed16
Esteban Oliveros <esteban.jof@gmail.com>
2015-06-06 08:19:47 -0430

    Negamax funcionando, falta resultado en tablas

--- negamax/negamax.cc
+++ negamax/negamax.cc
@@ -6,7 +6,7 @@
  * Cristian Medina
  */
 
-#include "othello_cut.h" // won't work correctly until .h is fixed!
+#include "othello_cut.h"
 #include <iostream>
 #include <climits>
 
@@ -16,21 +16,33 @@ using namespace std;
 
 const int sign[2]={-1,1};   // 0 is white, 1 is black
 int negamax(state_t node, int color) {
-	if ( node.terminal() ) {
-		//cout << "Found terminal node:\n";
-		//cout << node << endl;
-		//cout << "Whit value = " << sign[color] * node.value() << endl;
-		return sign[color] * node.value();
-	}
 
-	//cout << "It's not terminal" << endl;
+	/* Si el nodo es terminal, calculamos el valor de ese estado del juego
+	 * A diferencia del negamax original, no nos interesa la profundidad,
+	 * evaluaremos todo el sub-arbol.
+	 */
+	if ( node.terminal() ) 	return sign[color] * node.value();
+
 	int max = INT_MIN;
+	// Obtenemos los estado sucesores del presente juego (node)
 	vector<int> moves = node.get_succesors(color);
-	//cout << "It has = " << moves.size() << " succesors" << endl;
+	
+	/* Si no existen movimientos, esto quiere decir que el jugador pierde su
+	 * turno. Sin embargo, salir en este punto sería saltar un nivel del árbol
+	 * e invertir los roles de los jugadores, cosa que no queremos.
+	 * Por lo tanto, el valor para este juego y para el jugador/color actual
+	 * será el mismo que este juego pero para el jugador/color contrario
+	 */
+	if (moves.empty()) return -negamax(node, 1-color);
+
+	/* Por el contrario, si el juego y jugador/color actual si tiene movimientos
+	 * procedemos a calcular de manera recursiva negamax para cada uno
+	 * para obtener su valor, el cual es el max entre todos.
+	 */
 	for(vector<int>::iterator it = moves.begin(); it != moves.end(); it++){
 		state_t child = node.move(color, *it);
 		int x = -negamax(child, 1-color);
-		if ( x > max ) max = x;
+		if ( x > max ) max = x;		
 	}
 	
 	return max;
@@ -39,24 +51,22 @@ int negamax(state_t node, int color) {
 int main(int argc, const char **argv) {
 
 	state_t root;
-	for(int d = MAXMOVE; d > 0; d--){
+		
+	for(int d = MAXMOVE; d >= -1; d--){ // d >= -1 para que llegue a la raiz
+		cout << "----------------------------------------------" << endl;
 		state_t state(root);
 		bool player;
 		int pos;
-		cout << "Move secuence = ";
 		for( int i = 0; i <= d; ++i ) {
 			player = i % 2 == 0; // black moves first!
-			pos = PV[i];
+			pos = PV[i];			
 			state = state.move(player, pos);
-			cout << pos << " ";
 		}
-		cout << endl;
-		//cout << state;
-		//cout << "Next player = " << (1-player ? "Black" : "White") << endl;
-		//cout << "Value of the game = " << state.value() << endl;
-		//cout << "Last move = " << pos << endl;
-		cout << "Value of negamax = " << negamax(state,1-player) << endl;	  
-		cout << "----------------------------------------------" << endl;
+		cout << state;
+		// Negamax debe ser siempre -4
+		int nmax = sign[1-player]*negamax(state,1-player);
+		cout << "Value of negamax = " << nmax << endl;		
+		assert(nmax == -4);
 	}
 }
 
